494f13c77fd22010a3265a397793066dbec483b5
Oscar <oscarwilliam202@gmail.com>
2023-11-27 22:07:59 -0300

    Código incial do trabalho

---
+++ codigos/dg/fast/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django import forms
+
+class Imagem(models.Model):
+    imagem = models.ImageField(upload_to="imgs/", null=True, blank=True) 
+    descricao = models.TextField(blank=True)
+
+class ImagemForm(forms.ModelForm):
+    class Meta:
+        model = Imagem
+        fields = [
+            'Imagem',
+            'descricao'
+        ]
