4a586d072b196428082aa27fc5974f3ab47eb39f
Rayanne Figueiredo <rayanne4r@gmail.com>
2024-01-13 23:00:55 -0300

    Inclusão do css para a tela de redefinição de senha e algumas alterações no html dessa tela

--- codigos/fast/models.py
+++ codigos/fast/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django import forms
 
-# class Imagem(models.Model):
-#     imagem = models.ImageField(upload_to="imgs/", null=True, blank=True) 
-#     descricao = models.TextField(blank=True)
+class Imagem(models.Model):
+    imagem = models.ImageField(upload_to="imgs/", null=True, blank=True) 
+    descricao = models.TextField(blank=True)
 
-# class ImagemForm(forms.ModelForm):
-#     class Meta:
-#         model = Imagem
-#         fields = [
-#             'Imagem',
-#             'descricao'
-#         ]
+class ImagemForm(forms.ModelForm):
+    class Meta:
+        model = Imagem
+        fields = [
+            'Imagem',
+            'descricao'
+        ]
 
 
 
